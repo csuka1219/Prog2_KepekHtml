92c825760d37a2e0f087545cd9ce4ec38d221457
csuka1219 <csukabalazs1219@gmail.com>
2022-12-14 22:36:40 +0100

    Update Program.cs

--- ProjektCSharp/Program.cs
+++ ProjektCSharp/Program.cs
@@ -7,24 +7,46 @@ namespace ProjektCSharp
     {
         static void Main(string[] args)
         {
-            string workingDirectory = Environment.CurrentDirectory + "\\images";
-            string[] directories = Directory.GetDirectories(workingDirectory, "*", SearchOption.AllDirectories);
+            if(args.Length<2)
+            {
+                Console.WriteLine("Adjon meg egy elérési utat");
+                return;
+            }
+            string workingDirectory = args[1];
+            List<string> directories = Directory.GetDirectories(workingDirectory, "*", SearchOption.AllDirectories).ToList();
+            directories.Add(workingDirectory);
+            if (args.Length > 2 && args[2] == "clr")
+            {
+                ClearHtml(directories);
+                return;
+            }
             List<Folder> l = new List<Folder>();
-            l.Add(new Folder(workingDirectory, directories.ToList()));
+            l.Add(new Folder(workingDirectory, directories));
             foreach (var d in directories)
             {
-                l.Add(new Folder(d, directories.ToList()));
-                System.IO.DirectoryInfo di = new DirectoryInfo(d);
+                l.Add(new Folder(d, directories));
+                if (args.Length>2 && args[2]=="clr")
+                {
+                    DirectoryInfo di = new DirectoryInfo(d);
+                    foreach (FileInfo file in di.GetFiles().Where(x => x.FullName.EndsWith("html")))
+                    {
+                        file.Delete();
+                    }
+                }
+            }
+            Console.WriteLine();
+            Html.Create(l);
+        }
+        static void ClearHtml(List<string> directories)
+        {
+            foreach (var d in directories)
+            {
+                DirectoryInfo di = new DirectoryInfo(d);
                 foreach (FileInfo file in di.GetFiles().Where(x => x.FullName.EndsWith("html")))
                 {
                     file.Delete();
                 }
             }
-            Console.WriteLine();
-            Html.Create(l);
-            //string path = Directory.GetCurrentDirectory();
-            //Console.WriteLine(path);
-            //System.IO.DirectoryInfo di = new DirectoryInfo(path+"/images/sub05");
         }
     }
 }
